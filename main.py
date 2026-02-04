75c2dc80ad9bf3d97b08379146403037b8b638f7
alshara030 <mohamedalshara779@gmail.com>
2026-02-04 20:02:55 +0100

    Update main.py

--- main.py
+++ main.py
@@ -7,8 +7,7 @@ app = Flask(__name__)
 # --- CONFIGURATION (Change this to your actual ID later) ---
 # Your Publisher ID looks like: pub-1935984181885001"
 
-<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1935984181885001"
-     crossorigin="anonymous"></script> 
+
 
 # --- STYLES ---
 CSS = """
@@ -148,6 +147,7 @@ def download_file(vid):
 
 if __name__ == '__main__':
     app.run()
+
 
 
 
